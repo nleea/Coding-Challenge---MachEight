106ab9a42cd4d1084e5a02b2e4e801e2b338fd44
Nelson <neldecas12@gmail.com>
2023-04-25 18:05:18 -0500

    firts commit

---
+++ app.py
@@ -0,0 +1,12 @@
+def pairs_function(list_numbers, target):
+    complements = set()
+    pair = [(target - number, number) for number in set(list_numbers) if (target - number) in complements or complements.add(number)]
+    return pair
+
+with open("numbers.txt", "r") as numberText:
+    content = numberText.read()
+
+numbers = content.split()
+list_numbers = numbers[0].split(",")
+results_numbers = list(map(int, map(str.strip, list_numbers)))
+print(pairs_function(results_numbers,int(numbers[-1])))
